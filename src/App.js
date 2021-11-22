a61453c57addce854ff1b3f2ae409e3f8c5889fc
lebonYG <lebonganza@outlook.com>
2021-11-21 19:09:54 -0500

    Connect user wallet through Metamask

--- src/App.js
+++ src/App.js
@@ -1,20 +1,65 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'lebon_yg';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState()
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
+
+  //Check if wallet is connected
+  const checkIfWalletIsConnected = async () => {
+    const {ethereum} = window
+    if(!ethereum){
+      console.log('Please make sure you have an active Metamask extension in your browser')
+      return
+    }
+    console.log('Wallet connected', ethereum)
+
+    const accounts = await ethereum.request({method: 'eth_accounts'})
+
+    if(accounts.length !== 0){
+      const account = accounts[0]
+      console.log('Found an authorized account: ', account)
+      setCurrentAccount(account)
+    }
+    else {
+      console.log('No authorized account found!')
+    }
+  }
+
+
+  const connectWallet = async () => {
+    try{
+      const {ethereum} = window
+
+      if(!ethereum){
+        alert('Please install Metamask')
+        return
+      }
+
+      const accounts = await ethereum.request({method: 'eth_requestAccounts'})
+
+      console.log('Connected account: ', accounts[0])
+    }catch (e) {
+      console.log(e)
+    }
+
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected()
+  })
 
   return (
     <div className="App">
@@ -33,7 +78,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`brought to you by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
